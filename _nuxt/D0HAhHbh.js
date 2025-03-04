22e2744a7796bb0b85be839db522050dfd8b133f
bynicodevelop <32275195+bynicodevelop@users.noreply.github.com>
2025-03-04 14:47:00 +0000

    Deploying to main from @ bynicodevelop/visity.me@5ad624b9b2baa9cbc41d9d073a998ebb4612faef 🚀

--- _nuxt/D0HAhHbh.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"./XbrZPR0v.js";import{r as n}from"./6VV17LS1.js";const f=a("alertStore",()=>{const e=n([]);return{alerts:e,addAlert:(r,t,o="info",l=null)=>{e.value.push({id:Date.now(),title:r,message:t,type:o,duration:l??3e3})},removeAlert:r=>{e.value=e.value.filter(t=>t.id!==r)}}});export{f as u};
