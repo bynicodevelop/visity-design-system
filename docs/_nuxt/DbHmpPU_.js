ab4f8ec10d0d53ff2c8832ace3b4c5e501cfa262
bynicodevelop <32275195+bynicodevelop@users.noreply.github.com>
2025-03-04 14:24:55 +0000

    Deploying to main from @ bynicodevelop/visity.me@9c01c02508a448d83a2050e008c6132c35a9e69e 🚀

---
+++ docs/_nuxt/DbHmpPU_.js
@@ -0,0 +1,1 @@
+var o={};const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Introduction","Components","*"]}},staticDirs:{basePath:o.STORYBOOK_BASE_PATH||""}}};export{e as default};
