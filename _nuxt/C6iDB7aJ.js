14a2a3d274e881ef96a68458a0ff8ecf156d886e
bynicodevelop <32275195+bynicodevelop@users.noreply.github.com>
2025-03-04 06:30:50 +0000

    Deploying to main from @ bynicodevelop/visity.me@7bc5e6fff7703e20b4d1aa2c6f901aa1ca64cdbf 🚀

--- _nuxt/C6iDB7aJ.js
+++
@@ -1,9 +0,0 @@
-import{_ as n}from"./DxLqxJE9.js";import"./BASjsz4d.js";import"./DlAUqK2U.js";const i={title:"DesignSysteme/Primary",component:n,tags:["autodocs"],argTypes:{color:{control:"select",options:["primary","secondary","danger","warning","success"],description:"Button color variant"},size:{control:"select",options:["sm","md","lg"],description:"Button size"},readOnly:{control:"boolean",description:"Disabled state"},type:{control:"select",options:["button","submit","reset"],description:"Button HTML type"},label:{control:"text",description:"Button label"}}},t={args:{label:"Welcome Button",color:"primary",size:"md",readOnly:!1,type:"button"}};var o,e,r;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    label: 'Welcome Button',
-    color: 'primary',
-    size: 'md',
-    readOnly: false,
-    type: 'button'
-  }
-}`,...(r=(e=t.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const l=["PrimaryStory"];export{t as PrimaryStory,l as __namedExportsOrder,i as default};
