031613feae0601b1b4afb880286fc29b2c943535
bynicodevelop <32275195+bynicodevelop@users.noreply.github.com>
2025-03-05 11:03:42 +0000

    Deploying to main from @ bynicodevelop/visity.me@3092af3ec0c7192501dc7da2fc2d5c20b5e00301 🚀

---
+++ _nuxt/83YyQ98p.js
@@ -0,0 +1,17 @@
+import{_ as t}from"./BYevT2vT.js";import"./DYHvSiQ9.js";const c={title:"DesignSysteme/Pricing/ItemPrice",component:t,tags:["autodocs"],parameters:{layout:"centered",docs:{description:{component:"Le composant ItemPrice est utilisé pour afficher des informations de prix au sein du système de design."}}},render:n=>({components:{ItemPrice:t},setup(){return{args:n}},template:`
+      <div>
+      <ItemPrice
+        v-bind="args"
+      />
+      </div>
+    `}),argTypes:{title:{control:{type:"text"}},description:{control:{type:"text"}},price:{control:{type:"number"}},currency:{control:{type:"text"}},interval:{control:{type:"text"}},isPopular:{control:{type:"boolean"}},features:{control:{type:"array"}}}},e={args:{title:"Starter",description:"Description du prix pour le starter.",price:29,currency:"EUR",interval:"month",isPopular:!1,features:["1 User","1 Website","1 GB Storage","24/7 Support"]}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    title: 'Starter',
+    description: 'Description du prix pour le starter.',
+    price: 29,
+    currency: 'EUR',
+    interval: 'month',
+    isPopular: false,
+    features: ['1 User', '1 Website', '1 GB Storage', '24/7 Support']
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,c as default};
